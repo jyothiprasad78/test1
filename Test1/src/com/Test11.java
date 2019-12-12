5e3e584a3af253faf466d905956c61433438f723
jyothi <jyothi@jyothi-ThinkPad-L430>
2019-12-12 09:16:20 +0530

    commit3

--- Test1/src/com/Test11.java
+++ Test1/src/com/Test11.java
@@ -4,7 +4,7 @@ public class Test11 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("first print");
+System.out.println("first print and second time");
 	}
 
 }
